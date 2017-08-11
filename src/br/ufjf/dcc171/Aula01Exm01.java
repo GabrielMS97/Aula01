cd2709e68cacda272c402318e5f4a0967d2bdfd3
ice <ice@localhost>
2017-08-10 21:48:21 -0300

    Aula 01

---
+++ src/br/ufjf/dcc171/Aula01Exm01.java
@@ -0,0 +1,21 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JOptionPane;
+
+public class Aula01Exm01 {
+
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+        if (nome != null && !nome.isEmpty()) {
+            System.out.println(nome);
+            JOptionPane.showMessageDialog(null, "Bem vindo as GUIs",
+                    "Olá " + nome + "!", JOptionPane.PLAIN_MESSAGE);
+        } 
+        else {
+            JOptionPane.showMessageDialog(null, "Você deveria ter digitado um nome!",
+                    "ERRO! ", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
+}
